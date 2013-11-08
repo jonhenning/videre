3a4f4f5fbc8016d86d844dbd84b3dc3bc5edcb88
Jon Henning <jonhenning@gmail.com>
2013-11-08 16:09:11 -0600

    testing commit to branch

---
+++ Videre.Core/Extensions/Bootstrap/BootstrapHtmlExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Videre.Core.Extensions.Bootstrap
+{
+    public static class BootstrapHtmlExtension
+    {
+        public static Bootstrap<TModel> Bootstrap<TModel>(this HtmlHelper<TModel> helper)
+        {
+            return new Bootstrap<TModel>(helper);
+        }
+    }
+}
