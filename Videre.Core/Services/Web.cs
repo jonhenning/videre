79d6d86763b26ec80ec2a3e3cd907aecd0b05470
Jon Henning <jonhenning@gmail.com>
2013-11-18 14:17:30 -0600

    BS3 - removed embedded TextEditors from core, now just integrated via controltypes (webreferences), blog updated

--- Videre.Core/Services/Web.cs
+++ Videre.Core/Services/Web.cs
@@ -12,52 +12,9 @@ namespace Videre.Core.Services
             portalId = string.IsNullOrEmpty(portalId) ? Portal.CurrentPortalId : portalId;
             return new List<WebReference>()
             {
-                //new WebReference() { Name = "jQuery", Group = "jQuery", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/jquery-1.7.1/jquery-1.7.1.min.js" },
-                //new WebReference() { Name = "jQuery UI", Group = "jQuery UI", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/jquery-ui-1.8.18/js/jquery-ui-1.8.18.custom.min.js", DependencyGroups = new List<string>() {"jQuery"} },
-                //new WebReference() { Name = "jQuery UI css", Group = "jQuery UI", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.StyleSheetReference, PortalId = portalId, Url = "~/scripts/jquery-ui-1.8.18/css/smoothness/jquery-ui-1.8.18.custom.css" },
-                //new WebReference() { Name = "jsRender", Group = "jsRender", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/jsrender.js" },
-                //new WebReference() { Name = "bootstrap", Group = "bootstrap", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/bootstrap-2.1.0/js/bootstrap.js" },
-                //new WebReference() { Name = "bootstrap css", Group = "bootstrap css", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.StyleSheetReference, PortalId = portalId, Url = "~/scripts/bootstrap-2.1.0/css/bootstrap.min.css" },
-
                 new WebReference() { Name = "videre extensions", Group = "videre", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/videre.extensions.js", DependencyGroups = new List<string>() {"jQuery", "jQuery UI", "bootstrap", "jsRender"} , Sequence = 0 },
                 new WebReference() { Name = "videre", Group = "videre", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/videre.js", Sequence = 1 },
-                new WebReference() { Name = "videre css", Group = "videre", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.StyleSheetReference, PortalId = portalId, Url = "~/Content/videre.css" },
-                
-                //new WebReference() { Name = "datatables", Group = "datatables", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/DataTables-1.9.4/media/js/jquery.dataTables.min.js", DependencyGroups = new List<string>() {"jQuery"}, Sequence = 0  },
-                //new WebReference() { Name = "datatables bootstrap", Group = "datatables", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/DataTables-1.9.4/media/js/jquery.dataTables.bootstrap.js", Sequence = 1 },
-                //new WebReference() { Name = "datatables css", Group = "datatables", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.StyleSheetReference, PortalId = portalId, Url = "~/scripts/DataTables-1.9.4/media/css/jquery.dataTables.bootstrap.css" },
-
-                //new WebReference() { Name = "datatables zeroclipboard", Group = "datatables tabletools", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/DataTables-1.9.4/media/js/ZeroClipboard.js", DependencyGroups = new List<string>() {"datatables"}, Sequence = 0 },
-                //new WebReference() { Name = "datatables tabletools", Group = "datatables tabletools", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/DataTables-1.9.4/media/js/TableTools.min.js", Sequence = 1 },
-
-                //new WebReference() { Name = "prettify", Group = "prettify", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/prettify/prettify.js" },
-                //new WebReference() { Name = "prettify css", Group = "prettify", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.StyleSheetReference, PortalId = portalId, Url = "~/scripts/prettify/prettify.css" },
-                //new WebReference() { Name = "prettify startup", Group = "prettify", LoadType = WebReferenceLoadType.Startup, Type = WebReferenceType.Script, PortalId = portalId, Text = "prettyPrint();" },
-
-                //new WebReference() { Name = "dynatree", Group = "dynatree", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/dynatree1.1/jquery.dynatree.min.js", DependencyGroups = new List<string>() {"jQuery", "jQuery UI"}  },
-                //new WebReference() { Name = "dynatree css", Group = "dynatree", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.StyleSheetReference, PortalId = portalId, Url = "~/scripts/dynatree1.1/skin/ui.dynatree.css" },
-
-                //new WebReference() { Name = "timepicker", Group = "timepicker", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/jquery-ui-timepicker-1.0.1/jquery-ui-timepicker-addon.js", DependencyGroups = new List<string>() {"jQuery", "jQuery UI"} },
-                //new WebReference() { Name = "timepicker css", Group = "timepicker", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.StyleSheetReference, PortalId = portalId, Url = "~/scripts/jquery-ui-timepicker-1.0.1/jquery-ui-timepicker-addon.css", DependencyGroups = new List<string>() {"jQuery", "jQuery UI"} },
-
-                //new WebReference() { Name = "fileuploader", Group = "fileuploader", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/fileuploader.js", DependencyGroups = new List<string>() {"jQuery"} },
-
-                //new WebReference() { Name = "multiselect", Group = "multiselect", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/multiselect/jquery.multiselect.min.js", DependencyGroups = new List<string>() {"jQuery", "jQuery UI"} },
-                //new WebReference() { Name = "multiselect css", Group = "multiselect", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.StyleSheetReference, PortalId = portalId, Url = "~/scripts/multiselect/jquery.multiselect.css", DependencyGroups = new List<string>() {"jQuery", "jQuery UI"} },
-                //new WebReference() { Name = "multiselect startup", Group = "multiselect", LoadType = WebReferenceLoadType.Startup, Type = WebReferenceType.Script, PortalId = portalId, Text = "$('select[data-controltype=\"multiselect\"]').multiselect({ selectedList: 3 });" },
-
-                //new WebReference() { Name = "CLTextEditor", Group = "CLTextEditor", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/controls/core/CLTextEditor/CLEditor1_3_0/jquery.cleditor.min.js", DependencyGroups = new List<string>() {"jQuery"} },
-                //new WebReference() { Name = "CLTextEditor css", Group = "CLTextEditor", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.StyleSheetReference, PortalId = portalId, Url = "~/scripts/controls/core/CLTextEditor/CLEditor1_3_0/jquery.cleditor.css", DependencyGroups = new List<string>() {"jQuery"} },
-
-                //new WebReference() { Name = "CKTextEditor", Group = "CKTextEditor", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/controls/core/CKTextEditor/ckeditor_3.6.4/ckeditor.js", Sequence = 0, DependencyGroups = new List<string>() {"jQuery"} },
-                //new WebReference() { Name = "CKTextEditor jQuery adapter", Group = "CKTextEditor", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/controls/core/CKTextEditor/ckeditor_3.6.4/adapters/jquery.js", Sequence = 1, DependencyGroups = new List<string>() {"jQuery"} },
-
-                //new WebReference() { Name = "WYSIHTML5", Group = "WYSIHTML5TextEditor", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/controls/core/WYSIHTML5TextEditor/WYSIHTML5_0.2/lib/js/wysihtml5-0.3.0.min.js", Sequence = 0, DependencyGroups = new List<string>() {"jQuery"} },
-                //new WebReference() { Name = "WYSIHTML5 bootstrap", Group = "WYSIHTML5TextEditor", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/controls/core/WYSIHTML5TextEditor/WYSIHTML5_0.2/dist/bootstrap-wysihtml5-0.0.2.min.js", Sequence = 1, DependencyGroups = new List<string>() {"jQuery"} },
-                //new WebReference() { Name = "WYSIHTML5 css", Group = "WYSIHTML5TextEditor", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.StyleSheetReference, PortalId = portalId, Url = "~/scripts/controls/core/WYSIHTML5TextEditor/WYSIHTML5_0.2/dist/bootstrap-wysihtml5-0.0.2.css", DependencyGroups = new List<string>() {"jQuery"} },
-
-                new WebReference() { Name = "highcharts", Group = "highcharts", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.ScriptReference, PortalId = portalId, Url = "~/scripts/controls/core/highcharts/highcharts.js", DependencyGroups = new List<string>() {} }
-
+                new WebReference() { Name = "videre css", Group = "videre", LoadType = WebReferenceLoadType.Defer, Type = WebReferenceType.StyleSheetReference, PortalId = portalId, Url = "~/Content/videre.css" }
             };
         }
 
