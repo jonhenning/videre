7af5cef9ddab121d8f42e16c41886296c816c9a7
Jon Henning <jonhenning@gmail.com>
2014-01-06 11:58:03 -0600

    FIX: maxlength to IFluentBootstrapInputControl interface

--- Videre.Core/Extensions/Bootstrap/IFluentBootstrapInputControl.cs
+++ Videre.Core/Extensions/Bootstrap/IFluentBootstrapInputControl.cs
@@ -15,6 +15,7 @@ namespace Videre.Core.Extensions.Bootstrap
         TControl Required(bool required);
         TControl ReadOnly();
         TControl ReadOnly(bool readOnly);
+        TControl MaxLength(int? maxLength);
         TControl InputSize(Bootstrap.BootstrapUnits.InputSize size);
         TControl Append(Bootstrap.IBootstrapBaseControl ctl);
         TControl Prepend(Bootstrap.IBootstrapBaseControl ctl);
