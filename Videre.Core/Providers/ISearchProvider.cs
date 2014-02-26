80131e6641506c4295922c94caadca0f231b2d31
Jon Henning <jonhenning@gmail.com>
2014-02-26 13:58:06 -0600

    REFACTOR: move ISearchProvider to Providers folder.

---
+++ Videre.Core/Providers/ISearchProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lucene.Net.Index;
+
+//namespace Videre.Core.Providers
+//{
+//    public interface ISearchProvider
+//    {
+//        List<string> Generate(IndexWriter writer);
+//        bool IsAuthorized(Models.SearchDocument doc, string userId);
+//        Models.SearchResult FormatResult(Models.SearchDocument doc);
+
+//    }
+//}
+
+//TODO: refactor namespace
+namespace Videre.Core.Services
+{
+    //[Obsolete("Use Videre.Core.Providers.ISearchProvider instead")]
+    public interface ISearchProvider //: Videre.Core.Providers.ISearchProvider
+    {
+        List<string> Generate(IndexWriter writer);
+        bool IsAuthorized(Models.SearchDocument doc, string userId);
+        Models.SearchResult FormatResult(Models.SearchDocument doc);
+
+    }
+}
