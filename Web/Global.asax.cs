b5151bc45698771b596e2e3bfd34fab5ab315eda
Jon Henning <jonhenning@gmail.com>
2015-02-05 10:57:38 -0600

    ENH:  error handling modification to handle problems with Application_Start

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -57,27 +57,57 @@ namespace Videre.Web
 
         protected void Application_Start()
         {
-            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+            try
+            {
+                AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
 
-            Services.Logging.Logger.Debug("Application_Start");
-            Services.Update.WatchForUpdates();
-            Services.CacheTimer.Register();
-            Services.Search.RegisterForAutoUpdate();
+                Services.Logging.Logger.Debug("Application_Start");
+                Services.Update.WatchForUpdates();
+                Services.CacheTimer.Register();
+                Services.Search.RegisterForAutoUpdate();
 
-            Services.Update.Register();
-            AreaRegistration.RegisterAllAreas();    //todo: not really needed anymore!
+                Services.Update.Register();
+                AreaRegistration.RegisterAllAreas();    //todo: not really needed anymore!
 
-            ModelBinders.Binders.DefaultBinder = new JsonNetModelBinder();
+                ModelBinders.Binders.DefaultBinder = new JsonNetModelBinder();
 
-            RegisterGlobalFilters(GlobalFilters.Filters);
-            RegisterRoutes(RouteTable.Routes);
+                RegisterGlobalFilters(GlobalFilters.Filters);
+                RegisterRoutes(RouteTable.Routes);
 
+                Core.Services.Repository.Dispose(); //application start is not same httpcontext as first request
+            }
+            catch (Exception ex)
+            {
+                Application["ApplicationStartError"] = ex;  //set application state notifying that we failed...  will notify every request (BeginRequest) that our site is not running
+                Services.Logging.Logger.Error("Application_Start", ex);
+                //HttpRuntime.UnloadAppDomain();
+            }
+        }
 
-            Core.Services.Repository.Dispose(); //application start is not same httpcontext as first request
+        public void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (Application["ApplicationStartError"] != null)
+                showErrorPage("[Application_Start]", " ", (Exception)Application["ApplicationStartError"]);
+        }
 
-            //if (Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment.IsAvailable) 
-            //    System.Diagnostics.Trace.Listeners.Add(new Microsoft.WindowsAzure.Diagnostics.DiagnosticMonitorTraceListener());
+        private void showErrorPage(string controllerName, string actionName, Exception exception)
+        {
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "Error";
+            routeData.Values["action"] = "Index";
 
+            Response.StatusCode = 500;
+
+            // Avoid IIS7 getting involved
+            Response.TrySkipIisCustomErrors = true;
+
+            // Execute the error controller
+            IController errorsController = new Videre.Web.Controllers.ErrorController();
+            routeData.Values["exception"] = new HandleErrorInfo(exception, controllerName, actionName);
+
+            var wrapper = new HttpContextWrapper(Context);
+            var rc = new RequestContext(wrapper, routeData);
+            errorsController.Execute(rc);
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
@@ -87,7 +117,6 @@ namespace Videre.Web
 
         public void Application_End()
         {
-            //Log.Info("Application_End");
             //http://weblogs.asp.net/scottgu/archive/2005/12/14/433194.aspx
             HttpRuntime runtime = (HttpRuntime)typeof(System.Web.HttpRuntime).InvokeMember("_theRuntime", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.GetField, null, null, null);
 
@@ -96,19 +125,13 @@ namespace Videre.Web
 
             string shutDownMessage = (string)runtime.GetType().InvokeMember("_shutDownMessage", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField, null, runtime, null);
             string shutDownStack = (string)runtime.GetType().InvokeMember("_shutDownStack", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField, null, runtime, null);
-
-            if (!EventLog.SourceExists(".NET Runtime"))
-                EventLog.CreateEventSource(".NET Runtime", "Application");
-
-            EventLog log = new EventLog();
-            log.Source = ".NET Runtime";
-            log.WriteEntry(String.Format("\r\n\r\n_shutDownMessage={0}\r\n\r\n_shutDownStack={1}", shutDownMessage, shutDownStack), EventLogEntryType.Error);
+            Services.Logging.Logger.Error(String.Format("\r\n\r\n_shutDownMessage={0}\r\n\r\n_shutDownStack={1}", shutDownMessage, shutDownStack));
         }
 
 
         public void Application_Error(object sender, EventArgs e)
         {
-            //Log.Error("Application_Error", this.Server.GetLastError());
+            Services.Logging.Logger.Error("Application_Error", this.Server.GetLastError());
         }
 
         public void Application_EndRequest(object sender, EventArgs e)
@@ -125,4 +148,5 @@ namespace Videre.Web
         }
 
     }
+
 }
