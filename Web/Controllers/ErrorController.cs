cb09c506aa406fcd80fe70cbba3887cb62217d49
Jon Henning <jonhenning@gmail.com>
2015-02-05 11:10:13 -0600

    ENH:  added ErrorController turned off RAMMFAR by default

---
+++ Web/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Videre.Core.Services;
+using System.Web.Security;
+using Videre.Core.ActionResults;
+//using CodeEndeavors.Extensions;
+//using Videre.Core.Extensions;
+//using System.Diagnostics;
+
+namespace Videre.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index(HandleErrorInfo exception)
+        {
+            return View("Error", exception);
+        }
+
+    }
+}
