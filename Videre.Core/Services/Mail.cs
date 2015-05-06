9a75d4cdf761da381124af5e2544ef20cedf01d5
Jon Henning <jonhenning@gmail.com>
2015-05-06 13:40:55 -0500

    ENH: added few more overloads for mail send

--- Videre.Core/Services/Mail.cs
+++ Videre.Core/Services/Mail.cs
@@ -31,7 +31,7 @@ namespace Videre.Core.Services
                 var view = AlternateView.CreateAlternateViewFromString(body, null, "text/html");
                 foreach (var resource in linkedResources)
                     view.LinkedResources.Add(resource);
-                msg.AlternateViews.Add(view); 
+                msg.AlternateViews.Add(view);
             }
 
             if (attachments != null)
@@ -49,18 +49,28 @@ namespace Videre.Core.Services
             return Send(from, recipients, subject, body, isBodyHtml, linkedResources, null);
         }
 
+
         public static bool Send(string from, string recipients, string templateName, string subjectTemplateText, string bodyTemplateText, Dictionary<string, object> tokens, bool isBodyHtml = true, List<LinkedResource> linkedResources = null)
+        {
+            return Send(from, recipients, templateName, subjectTemplateText, bodyTemplateText, tokens, isBodyHtml, linkedResources, null);
+        }
+
+        public static bool Send(string from, string recipients, string templateName, string subjectTemplateText, string bodyTemplateText, Dictionary<string, object> tokens, bool isBodyHtml, List<LinkedResource> linkedResources, List<Attachment> attachments)
         {
             var expandedSubject = DynamicContent.ExpandTemplate("FastTemplate", templateName + "_subject", subjectTemplateText, tokens);
             var expandedBody = DynamicContent.ExpandTemplate("FastTemplate", templateName + "_body", bodyTemplateText, tokens);
-            return Send(from, recipients, expandedSubject, expandedBody, isBodyHtml, linkedResources);
+            return Send(from, recipients, expandedSubject, expandedBody, isBodyHtml, linkedResources, attachments);
         }
 
         public static bool Send(string from, string recipients, string subjectTemplateFileName, string bodyTemplateFileName, Dictionary<string, object> tokens, bool isBodyHtml = true, List<LinkedResource> linkedResources = null)
         {
+            return Send(from, recipients, subjectTemplateFileName, bodyTemplateFileName, tokens, isBodyHtml, linkedResources, null);
+        }
+        public static bool Send(string from, string recipients, string subjectTemplateFileName, string bodyTemplateFileName, Dictionary<string, object> tokens, bool isBodyHtml, List<LinkedResource> linkedResources, List<Attachment> attachments)
+        {
             var expandedSubject = DynamicContent.ExpandTemplateFile("FastTemplate", subjectTemplateFileName + "_subject", subjectTemplateFileName, tokens);
             var expandedBody = DynamicContent.ExpandTemplateFile("FastTemplate", bodyTemplateFileName + "_body", bodyTemplateFileName, tokens);
-            return Send(from, recipients, expandedSubject, expandedBody, isBodyHtml, linkedResources);
+            return Send(from, recipients, expandedSubject, expandedBody, isBodyHtml, linkedResources, attachments);
         }
     }
 }
