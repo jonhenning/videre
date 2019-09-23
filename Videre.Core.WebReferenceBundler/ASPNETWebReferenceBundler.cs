84694ba999d7c1c85cdf8c77e2d0da82efec8360
Jon Henning <jonhenning@gmail.com>
2019-09-23 10:00:45 -0500

    ENH: css bundling support for references

--- Videre.Core.WebReferenceBundler/ASPNETWebReferenceBundler.cs
+++ Videre.Core.WebReferenceBundler/ASPNETWebReferenceBundler.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Optimization;
 using Videre.Core.Extensions;
 
@@ -48,7 +51,13 @@ namespace Videre.Core.WebReferenceBundler
                 {
                     var hash = String.Join("~", list.Items.Select(i => i.Src)).GetHashCode();
                     var src = "~/Content/_" + hash;
-                    var bundle = new StyleBundle(src).Include(list.Items.Select(i => "~/" + i.Src).ToArray());
+                    var bundle = new StyleBundle(src);
+                    bundle.Transforms.Add(new CssRewriteUrlTransform2());
+                    bundle.Include(list.Items.Select(i => "~/" + i.Src).ToArray());
+                    //list.Items.Select(i => "~/" + i.Src).ToList().ForEach(i =>
+                    //{
+                    //    bundle.Include(i, new CssRewriteUrlTransform2());
+                    //});
 
                     bundle.Orderer = new PassthruBundleOrderer();
                     BundleTable.Bundles.Add(bundle);
@@ -75,4 +84,55 @@ namespace Videre.Core.WebReferenceBundler
             
         }
     }
+
+    //https://gunnarpeipman.com/aspnet-mvc-bundling/
+    public class CssRewriteUrlTransform2 : IBundleTransform
+    {
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            Regex pattern = new Regex(@"url\s*\(\s*([""']?)([^:)]+)\1\s*\)", RegexOptions.IgnoreCase);
+
+            response.Content = string.Empty;
+
+            // open each of the files
+            foreach (BundleFile bfile in response.Files)
+            {
+                var file = bfile.VirtualFile;
+                using (var reader = new StreamReader(file.Open()))
+                {
+
+                    var contents = reader.ReadToEnd();
+
+                    // apply the RegEx to the file (to change relative paths)
+                    var matches = pattern.Matches(contents);
+
+                    if (matches.Count > 0)
+                    {
+                        var directoryPath = VirtualPathUtility.GetDirectory(file.VirtualPath);
+
+                        foreach (Match match in matches)
+                        {
+                            if (match.Value.IndexOf("url(//") == -1)    //if external url don't replace
+                            {
+                                // this is a path that is relative to the CSS file
+                                var imageRelativePath = match.Groups[2].Value;
+
+                                // get the image virtual path
+                                var imageVirtualPath = VirtualPathUtility.Combine(directoryPath, imageRelativePath);
+
+                                // convert the image virtual path to absolute
+                                var quote = match.Groups[1].Value;
+                                var replace = String.Format("url({0}{1}{0})", quote, VirtualPathUtility.ToAbsolute(imageVirtualPath));
+                                contents = contents.Replace(match.Groups[0].Value, replace);
+                            }
+                        }
+
+                    }
+                    // copy the result into the response.
+                    response.Content = String.Format("{0}\r\n{1}", response.Content, contents);
+                }
+            }
+        }
+    }
+
 }
