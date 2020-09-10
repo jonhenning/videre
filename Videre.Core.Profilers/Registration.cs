5974a3f2e9e5f5b705eaa0d2b6a84efa87d83d00
Jon Henning <jonhenning@gmail.com>
2020-09-10 11:30:17 -0500

    profiler plugin

---
+++ Videre.Core.Profilers/Registration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Videre.Core.Models;
+using Videre.Core.Services;
+
+namespace Videre.Core.Profilers
+{
+    public class Registration : IWidgetRegistration
+    {
+        public int Register()
+        {
+            int updates = 0;
+            updates += Update.Register(new List<Models.WidgetManifest>()
+            {
+                new Models.WidgetManifest() { Path = "Core/Profilers", Name = "MiniProfilerScripts", Title = "Mini Profiler Scripts", Category = "Profilers" },
+            });
+            return updates;
+        }
+
+        public int RegisterPortal(string portalId)
+        {
+            int updates = 0;
+            updates += Update.Register(new List<Models.SecureActivity>()
+            {
+                new Models.SecureActivity() { PortalId = portalId, Area = "Profiler", Name = "MiniProfiler", RoleIds = new List<string>() {Update.GetAdminRoleId(portalId)} }
+            });
+
+            return updates;
+        }
+    }
+}
